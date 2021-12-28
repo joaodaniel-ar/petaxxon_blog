cc133f33754bb4201efc113fc3c397567873312b
João Daniel <joaodaniel.tn@gmail.com>
2021-12-28 11:30:48 -0300

    Update Login Component

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UserController extends Controller
 {
@@ -13,5 +14,15 @@ class UserController extends Controller
         $user->email=$request->email;
         $user->password=bcrypt($request->password);
         $user->save();
+    }
+
+    public function login(){
+        $credentials = request(['name', 'password']);
+
+        if ($token = auth()->attempt($credentials)) {
+            return response()->json(['status' => 'success', 'token'=>$token]);
+        }
+
+        return response()->json(['status' => 'error']);
     }
 }
